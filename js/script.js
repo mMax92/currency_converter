096abc93ff3a970ab6af4b451b92d9caa61dd710
MMax <mr.moshnyatskiy@gmail.com>
2023-05-12 15:59:06 +0300

    Practice of XMLHttp Request

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const inputUAH = document.querySelector('#uah'),
+      inputUSD = document.querySelector('#usd');
+
+inputUAH.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current1.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+    request.send();
+
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.response);
+            const data = JSON.parse(request.response);
+            inputUSD.value = (+inputUAH.value / data.current.usd).toFixed(2);
+        } else {
+            inputUSD.value = "Something went wrong";
+        }
+    });
+});
